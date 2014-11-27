c6ae54e49188c735bdab62b27defb2e25d966cfe
Igor Kurochkin <ikurochkin@express42.com>
2014-11-27 18:01:48 +0300

    Update license and platforms

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name             'keepalived'
 maintainer       'Express42'
 maintainer_email 'info@express42.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures keepalived'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.3'
+
+supports         'debian'
+supports         'ubuntu'
