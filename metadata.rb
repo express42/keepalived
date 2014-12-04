6e0a5e6552c6d01d94f1ac37a284a6fd58dfad91
Igor Kurochkin <ikurochkin@express42.com>
2014-12-04 16:45:05 +0300

    Version bump to 1.0.4

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,11 @@ name             'keepalived'
 maintainer       'Express42'
 maintainer_email 'info@express42.com'
 license          'MIT'
-description      'Installs/Configures keepalived'
+description      'Installs and configures keepalived'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.0.4'
+
+recipe           'keepalived::default', 'Installs and configures keepalived.'
 
 supports         'debian'
 supports         'ubuntu'
