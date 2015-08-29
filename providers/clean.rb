8d385c75d7024db96e17ee049706140bc12bf0bc
Igor Kurochkin <ikurochkin@express42.com>
2015-08-29 14:48:50 +0300

    Fix rubocop issues

--- providers/clean.rb
+++ providers/clean.rb
@@ -1,39 +1,39 @@
- action :run do
-   conf_path = node['keepalived']['confpath']
+action :run do
+  conf_path = node['keepalived']['confpath']
 
-   # get list of files from conf dir
-   files_list = []
-   Dir.glob(conf_path + '/*').each { |path| files_list.push(path.sub(conf_path + '/', '')) }
+  # get list of files from conf dir
+  files_list = []
+  Dir.glob(conf_path + '/*').each { |path| files_list.push(path.sub(conf_path + '/', '')) }
 
-   resources_list = []
+  resources_list = []
 
-   # get file names from resource_collection
-   run_context.resource_collection.each do |resource|
-     if resource.class == Chef::Resource::KeepalivedVirtualServer
-       resources_list.push('vs_' + resource.name)
-     end
-     if resource.class == Chef::Resource::KeepalivedVrrp
-       resources_list.push('vrrp_' + resource.name)
-     end
-     if resource.class == Chef::Resource::KeepalivedCheckScript
-       resources_list.push('script_' + resource.name)
-     end
-   end
+  # get file names from resource_collection
+  run_context.resource_collection.each do |resource|
+    if resource.class == Chef::Resource::KeepalivedVirtualServer
+      resources_list.push('vs_' + resource.name)
+    end
+    if resource.class == Chef::Resource::KeepalivedVrrp
+      resources_list.push('vrrp_' + resource.name)
+    end
+    if resource.class == Chef::Resource::KeepalivedCheckScript
+      resources_list.push('script_' + resource.name)
+    end
+  end
 
-   resource_files = []
+  resource_files = []
 
-   # get list of files created by provider
-   resources_list.each do |r|
-     files_list.each do |f|
-       resource_files.push(f) if f.match(r)
-     end
-   end
+  # get list of files created by provider
+  resources_list.each do |r|
+    files_list.each do |f|
+      resource_files.push(f) if f.match(r)
+    end
+  end
 
-   # delete unnecessary files
-   (files_list - resource_files).each do |f|
-     ::File.unlink(conf_path + '/' + f)
-     Chef::Log.info('Removing file ' + f + ' in directory ' + conf_path + ' ...')
-   end
+  # delete unnecessary files
+  (files_list - resource_files).each do |f|
+    ::File.unlink(conf_path + '/' + f)
+    Chef::Log.info('Removing file ' + f + ' in directory ' + conf_path + ' ...')
+  end
 
-   new_resource.updated_by_last_action(true)
- end
+  new_resource.updated_by_last_action(true)
+end
